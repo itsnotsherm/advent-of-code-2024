2ab42ef5214ab9a90e3453c8c906cf4e7b38a1f8
Sherman <sherman.ho@u.nus.edu>
2024-12-01 14:59:24 +0800

    Add code for Day 1 Part 2

--- day1/day1.py
+++ day1/day1.py
@@ -21,5 +21,27 @@ def sum_of_sorted_distances(pathfile):
 
     return sum
 
+def similarity_score(pathfile):
+    f = open(pathfile, "r")
+
+    left_list = []
+    right_list = []
+
+    for line in f:
+        numbers = line.split("   ")
+        left_list.append(int(numbers[0]))
+        right_list.append(int(numbers[1]))
+
+    f.close()
+
+    score = 0
+    for n in left_list:
+        score += n * right_list.count(n)
+    
+    return score
+
 print(sum_of_sorted_distances("day1/input.txt"))
 print(sum_of_sorted_distances("day1/test.txt"))
+
+print(similarity_score("day1/input.txt"))
+print(similarity_score("day1/test.txt"))
